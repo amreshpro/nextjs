ec124225e9e660bccfc1d231281f405376e8b832
Amresh Maurya <amresh.terminal@gmail.com>
2023-10-30 22:14:34 +0530

    react2

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,17 +1,42 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 
 const SignupPage = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter()
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
 
+
+  const [buttonDisabled,setButtonDisabled] = useState({});
+
+useEffect(()=>{
+if(user.email.length>0 && user.password.length>0 && user.username.length>0){
+  setButtonDisabled(false);
+}
+else{
+  setButtonDisabled(true);
+}
+},[user])
+
   // on submit form
-  const onSignup = async () => {};
+  const onSignup = async () => {
+
+    try {
+      
+    } catch (error) {
+      
+    }
+
+
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -51,9 +76,9 @@ const SignupPage = () => {
       className="py-2 px-2  focus:outline-none focus:border-gray-700 rounded-lg"
     />
 
-k
 
-<button onClick={onSignup} className=" mt-4 p-2 border border-gray-300  rounded-lg  mb-4 focus:outline-none focus:border-gray-600 ">Signup</button>
+
+<button onClick={onSignup} className=" mt-4 p-2 border border-gray-300  rounded-lg  mb-4 focus:outline-none focus:border-gray-600 ">{buttonDisabled ? "No Signup":"Singup"}</button>
     
  <p> Already a user  <Link href="/login" className="text-blue-400 underline">Login</Link></p>
     </div>
